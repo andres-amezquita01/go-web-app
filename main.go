faa26b65d0a650658f04303e2f415fb53b0c9349
Andres Amezquita <andresfelipe.amezquita01@gmail.com>
2023-04-27 02:26:33 -0500

    Add sum

--- main.go
+++ main.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func helloWorldPage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "hello linux!!!")
+// Define la estructura para la solicitud de sumar dos números
+type AddRequest struct {
+	Num1 int `json:"num1"`
+	Num2 int `json:"num2"`
 }
 
+// Define la estructura para la respuesta de la suma de dos números
+type AddResponse struct {
+	Result int `json:"result"`
+}
+
+// Función para sumar dos números
+func AddNumbers(w http.ResponseWriter, r *http.Request) {
+	// Decodifica la solicitud JSON y la almacena en la variable "request"
+	var request AddRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Realiza la suma de los dos números
+	result := request.Num1 + request.Num2
+
+	// Codifica la respuesta en JSON y la envía como respuesta
+	response := AddResponse{Result: result}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+func ShowAddNumbers(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "You are going to add two numbers!")
+
+	// Decodifica la solicitud JSON y la almacena en la variable "request"
+}
 func main() {
-	http.HandleFunc("/hello", helloWorldPage)
-	err := http.ListenAndServe(":8080", nil)
-	fmt.Print("Error abc: ", err)
-	//http.ListenAndServe("", nil)
+	// Crea un nuevo enrutador "mux"
+	r := mux.NewRouter()
+
+	// Configura la ruta y el controlador para la función AddNumbers
+	r.HandleFunc("/add", AddNumbers).Methods("POST")
+	r.HandleFunc("/add", ShowAddNumbers).Methods("GET")
+	// Inicia el servidor en el puerto 8000
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
